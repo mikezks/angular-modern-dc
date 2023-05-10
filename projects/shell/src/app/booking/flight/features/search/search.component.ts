b4236a35c93b2c6e39ec5e699d94d1cfeae8476f
mikezks <23002661+mikezks@users.noreply.github.com>
2023-05-10 11:36:51 +0200

    feat: zoneless signal-based change detection

--- projects/shell/src/app/booking/flight/features/search/search.component.ts
+++ projects/shell/src/app/booking/flight/features/search/search.component.ts
@@ -1,10 +1,11 @@
 import { AsyncPipe, DatePipe, JsonPipe, NgFor, NgIf } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, effect, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { injectBookingFeature } from '../../../+state/booking.state';
 import { CardComponent } from '../../ui/card.component';
+import { zoneless } from '@angular-architects/demo/signals-zoneless';
 
 @Component({
   selector: 'app-flight-search',
@@ -18,15 +19,25 @@ import { CardComponent } from '../../ui/card.component';
   templateUrl: './search.component.html'
 })
 export class SearchComponent {
-  from = signal('Paris');
-  to = signal('London');
+  from = zoneless.signal('Paris');
+  to = zoneless.signal('London');
   basket: Record<number, boolean> = {
     3: true,
     5: true,
   };
   bookingFeature = injectBookingFeature();
-  flights = toSignal(
-    this.bookingFeature.flights$,
-    { initialValue: [] }
+  flights = zoneless.toSignal(
+    this.bookingFeature.flights$
   );
+  cd = inject(ChangeDetectorRef);
+
+  constructor() {
+    zoneless.effect(() => {
+      this.from();
+      this.to();
+      this.flights();
+
+      this.cd.detectChanges();
+    });
+  }
 }
